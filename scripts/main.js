307e44ed39bb8755311b7a4fc50bbb2cec0fe401
Jingyu-An <103467264+Jingyu-An@users.noreply.github.com>
2022-08-12 13:13:33 -0400

    modify space

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,28 @@
 (function (window, document) {
-  'use strict';
+    'use strict';
 
-  const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
-  const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
-  
-  $toggleBtn.addEventListener('click', function () {
-    toggleElements();
-  });
+    const $toggles = document.querySelectorAll('.toggle'); // Return NodeList
+    const $toggleBtn = document.getElementById('toggle-btn'); // Return Element
 
-  window.addEventListener('resize', function () {
-    if (window.innerWidth > 1024) {
-      offElements();
+    $toggleBtn.addEventListener('click', function () {
+        toggleElements();
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 1024) {
+            offElements();
+        }
+    });
+
+    function toggleElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.toggle('on');
+        });
     }
-  });
 
-  function toggleElements() {
-    [].forEach.call($toggles, function (toggle) {
-      toggle.classList.toggle('on');
-    });
-  }
-
-  function offElements() {
-    [].forEach.call($toggles, function (toggle) {
-      toggle.classList.remove('on');
-    });
-  }
+    function offElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
+        });
+    }
 })(window, document)
